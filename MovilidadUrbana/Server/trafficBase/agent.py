fb4dc8fce7ee62d5d5d7bd6dc6984956a6f57c36
mtumalan <mau@tumalan.com>
2023-11-22 09:09:09 -0600

    se añade simulación de tráfico en mesa

---
+++ MovilidadUrbana/Server/trafficBase/agent.py
@@ -0,0 +1,218 @@
+from mesa import Agent
+import heapq
+
+def heuristic(a, b):
+    """
+    Returns the Manhattan distance between two cells.
+    """
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def a_star_search(graph, start, goal, pathclear):
+    """
+    Finds the shortest path between two cells using A*.
+    """
+    print(f"Starting A* search from {start} to {goal}")
+    obstacles = [] # Priority queue
+    heapq.heappush(obstacles, (0, start)) # Add start cell to queue
+    wherefrom = {start: None} # Dictionary to keep track of where the path came from
+    sofar = {start: 0} # Dictionary to keep track of the cost so far
+    
+    while not len(obstacles) == 0:
+        current = heapq.heappop(obstacles)[1] # Get the cell with the lowest cost
+        #print(f"Checking cell: {current}")
+        if current == goal: # If the goal is reached, stop
+            #print("Goal reached in A* search")
+            break
+        for next in graph.get_neighborhood(current, moore=False, include_center=False): # Get the neighbors of the current cell
+            if not pathclear(current, next): # If the path is not clear,
+                continue # Skip this neighbor
+            new_cost = sofar[current] + 1 # Calculate the cost to get to the neighbor
+            if next not in sofar or new_cost < sofar[next]: # If the neighbor has not been visited or the cost is lower than the previous cost,
+                sofar[next] = new_cost # Update the cost
+                priority = new_cost + heuristic(goal, next) # Calculate the priority
+                heapq.heappush(obstacles, (priority, next)) # Add the neighbor to the queue
+                wherefrom[next] = current # Update the path
+                
+    path = {} # Dictionary to keep track of the path
+    current = goal # Start at the goal
+    while current != start: # While the start is not reached,
+        if current in wherefrom: # If the current cell is in the path,
+            prev = wherefrom[current] # Get the previous cell
+            path[prev] = current  # Map predecessor to current position
+            current = prev # Update the current cell
+        else: # If the current cell is not in the path,
+            print("No path found in A* search")
+            return {}  # Return empty path if no path is found
+
+    print(f"Path found: {path}")
+    return path # Return the path
+
+class Car(Agent):
+    """
+    Agent that moves towards a destination using A*.
+    """
+    def __init__(self, unique_id, model, spawn, destination = None):
+        """
+        Creates a new random agent.
+        Args:
+            unique_id: The agent's ID
+            model: Model reference for the agent
+            spawn: Where the agent is spawned
+            destination: Where the agent is going
+        """
+        super().__init__(unique_id, model)
+        self.destination = destination
+        self.spawn = spawn
+        self.path = None
+    
+    def initialize_path(self):
+        """
+        Initializes the path for the agent.
+        """
+        self.path = self.find_path()
+
+    def is_direction_valid(self, current_pos, next_pos, road_direction):
+        """
+        Checks if moving from current_pos to next_pos is valid based on the road_direction.
+        !!!Agregar que solo se puedan mover en diagonal si hay movimiento en la direccion permitida!!!
+        """
+        dx = next_pos[0] - current_pos[0] # Change in x
+        dy = next_pos[1] - current_pos[1] # Change in y
+        if road_direction == "Right": # If the road is going right,
+            return dx != -1 # Moving left is not allowed
+        elif road_direction == "Left": # If the road is going left,
+            return dx != 1 # Moving right is not allowed
+        elif road_direction == "Up": # If the road is going up,
+            return dy != -1 # Moving down is not allowed
+        elif road_direction == "Down": # If the road is going down,
+            return dy != 1 # Moving up is not allowed
+        return False # If the road direction is invalid, return False
+
+    def find_path(self):
+        """
+        Finds the path from spawn to destination.
+        """
+        if self.destination: # If the destination is set,
+            print(f"Finding path for Car {self.unique_id} from {self.spawn} to {self.destination}")
+            def pathclear(current, next): # Function to check if the path is clear
+                cell_contents = self.model.grid.get_cell_list_contents([next]) # Get the contents of the next cell
+
+                # Check if the next cell contains an obstacle
+                if any(isinstance(agent, Obstacle) for agent in cell_contents):
+                    #print(f"Obstacle found at {next}")  # Debug
+                    return False
+
+                # Check if the next cell is a road, traffic light, or destination
+                if any(isinstance(agent, (Road, Traffic_Light, Destination)) for agent in cell_contents):
+                    # If it's a road, check the direction
+                    roads = [agent for agent in cell_contents if isinstance(agent, Road)]
+                    if roads: # If there is a road in the cell
+                        road = roads[0]  # Get the first road
+                        return self.is_direction_valid(current, next, road.direction) # Check if the direction is valid
+                    return True  # If it's a traffic light or destination, movement is allowed
+                return False  # Cell without road, traffic light, or destination is not valid
+            return a_star_search(self.model.grid, self.spawn, self.destination, pathclear) # Find the path
+        print("No destination set for Car, no path to find")  # in case the destination is not reachable or not able to be set
+        return None # If the destination is not set, return None
+
+
+    def move(self):
+        """ 
+        Determines if the agent can move in the direction that was chosen
+        """        
+        if self.path and self.pos in self.path: # If the path is set and the current position is in the path,
+            next_pos = self.path.get(self.pos) # Get the next position
+            #print(f"Current Position: {self.pos}, Next Position: {next_pos}, Path: {self.path}")
+            if next_pos is not None: # If the next position is not None,
+                self.model.grid.move_agent(self, next_pos) # Move the agent to the next position
+                self.direction = self.get_direction(self.pos, next_pos) # Get the direction the agent should face
+                if next_pos == self.destination: # If the destination is reached,
+                    print(f"Car {self.unique_id} reached destination {self.destination}") 
+                    self.model.remove_car(self) # Remove the car from the model
+            else: # If the next position is None,
+                print("No valid next position found.") 
+        
+    def get_direction(self, current_pos, next_pos):
+        """
+        Determines the direction the agent should face after moving.
+        """
+        dx = next_pos[0] - current_pos[0] # Change in x
+        dy = next_pos[1] - current_pos[1] # Change in y
+        if dx == 1: # If the agent moved right,
+            return "Right"
+        elif dx == -1: # If the agent moved left,
+            return "Left"
+        elif dy == -1: # If the agent moved up,
+            return "Up"
+        elif dy == 1: # If the agent moved down,
+            return "Down"
+        else: # If the agent didn't move,
+            return None
+
+    def step(self): 
+        """ 
+        Determines the new direction it will take, and then moves
+        """
+        self.move()
+
+class Traffic_Light(Agent):
+    """
+    Traffic light. Where the traffic lights are in the grid.
+    """
+    def __init__(self, unique_id, model, state = False, timeToChange = 10):
+        super().__init__(unique_id, model)
+        """
+        Creates a new Traffic light.
+        Args:
+            unique_id: The agent's ID
+            model: Model reference for the agent
+            state: Whether the traffic light is green or red
+            timeToChange: After how many step should the traffic light change color 
+        """
+        self.state = state
+        self.timeToChange = timeToChange
+
+    def step(self):
+        """ 
+        To change the state (green or red) of the traffic light in case you consider the time to change of each traffic light.
+        """
+        if self.model.schedule.steps % self.timeToChange == 0:
+            self.state = not self.state
+
+class Destination(Agent):
+    """
+    Destination agent. Where each car should go.
+    """
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+    def step(self):
+        pass
+
+class Obstacle(Agent):
+    """
+    Obstacle agent. Just to add obstacles to the grid.
+    """
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+    def step(self):
+        pass
+
+class Road(Agent):
+    """
+    Road agent. Determines where the cars can move, and in which direction.
+    """
+    def __init__(self, unique_id, model, direction= "Left"):
+        """
+        Creates a new road.
+        Args:
+            unique_id: The agent's ID
+            model: Model reference for the agent
+            direction: Direction where the cars can move
+        """
+        super().__init__(unique_id, model)
+        self.direction = direction
+
+    def step(self):
+        pass
---
+++ MovilidadUrbana/Server/trafficBase/model.py
@@ -0,0 +1,100 @@
+from mesa import Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+from agent import *
+import json
+
+class CityModel(Model):
+    """ 
+        Creates a model based on a city map.
+
+        Args:
+            N: Number of agents in the simulation
+    """
+    def __init__(self, N):
+
+        # Load the map dictionary. The dictionary maps the characters in the map file to the corresponding agent.
+        dataDictionary = json.load(open("city_files/mapDictionary.json"))
+
+        self.traffic_lights = []
+
+        # Load the map file. The map file is a text file where each character represents an agent.
+        with open('city_files/2022_base.txt') as baseFile:
+            lines = baseFile.readlines()
+            self.width = len(lines[0])-1
+            self.height = len(lines)
+
+            self.grid = MultiGrid(self.width, self.height, torus = False) 
+            self.schedule = RandomActivation(self)
+
+            # Goes through each character in the map file and creates the corresponding agent.
+            for r, row in enumerate(lines):
+                for c, col in enumerate(row):
+                    if col in ["v", "^", ">", "<"]:
+                        agent = Road(f"r_{r*self.width+c}", self, dataDictionary[col])
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+
+                    elif col in ["S", "s"]:
+                        agent = Traffic_Light(f"tl_{r*self.width+c}", self, False if col == "S" else True, int(dataDictionary[col]))
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+                        self.schedule.add(agent)
+                        self.traffic_lights.append(agent)
+
+                    elif col == "#":
+                        agent = Obstacle(f"ob_{r*self.width+c}", self)
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+
+                    elif col == "D":
+                        agent = Destination(f"d_{r*self.width+c}", self)
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+                        self.schedule.add(agent)
+
+        self.num_agents = N
+        self.running = True
+        self.step_count = 0
+        
+    def set_destination(self):
+        """
+        Set the destination of the car.
+        """
+        destinations = [] # List of all the destinations
+        for agent in self.schedule.agents: # Iterate through all the agents
+            if isinstance(agent, Destination): # If the agent is a destination,
+                destinations.append(agent.pos) # Add the position of the destination to the list of destinations
+        #print(f"Number of destinations: {len(destinations)}")
+        return self.random.choice(destinations) if destinations else None # Return a random destination if there are any destinations, otherwise return None
+
+    def create_car(self):
+        """
+        Create cars agent and add them to the model.
+        """
+        corners = [(0, 0), (self.width-1, 0), (0, self.height-2), (self.width-1, self.height-2)] # List of all the corners of the grid
+        for corner in corners: # Iterate through all the corners
+            print(f"Placing car at: {corner}")
+            if 0 <= corner[0] < self.height and 0 <= corner[1] < self.width: # If the corner is valid,
+                destination = self.set_destination()  # Set the destination of the car
+                #print(f"Destination: {destination}")
+                agent = Car(f"Car_{self.num_agents + 1}", self, corner, destination)  # Create a unique ID for the car, and pass the model and the destination
+                self.grid.place_agent(agent, corner)  # Place the car on the grid
+                agent.initialize_path()  # Initialize the path after placing the car
+                self.schedule.add(agent)  # Add the car to the scheduler
+                self.num_agents += 1  # Increment the number of agents
+            else: # If the corner is invalid,
+                print(f"Invalid corner: {corner}")
+    
+    def remove_car(self, agent):
+        """
+        Remove a car from the model.
+        """
+        self.schedule.remove(agent) # Remove the agent from the scheduler
+        self.grid.remove_agent(agent) # Remove the agent from the grid
+
+
+    def step(self):
+        '''Advance the model by one step.'''
+        self.step_count += 1 # Increment the step count
+        if self.step_count == 1:
+            self.create_car() # Create cars at the beginning of the simulation
+        if self.step_count % 10 == 0:
+            self.create_car()  # Create new cars every 10 steps
+        self.schedule.step()
